15b131bc978c674f4270dc77f32f56a36347cddb
Saqlain1786 <shaikhsaqlain1786@gmail.com>
2026-04-27 16:07:02 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use('/api/attendance', require('./routes/attendanceRoutes'));
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
 
